f935ca3538a932c7dab99c551d662655f5ccec0e
vorobjovsh <vorobjov.sh@mail.ru>
2018-05-18 22:50:03 +0300

    added public step 3

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -102,8 +102,9 @@ btnTrashArr.addEventListener("click", infoAdmin);
 function addEmploy(event) {
     event.preventDefault();
     var elem = event.target;
+    var wicon = $(window).width();
     //console.log(elem);
-    if (elem.classList.contains("uservisible-str__sublink--imgman")) {
+    if (elem.classList.contains("uservisible-str__sublink--imgman") && wicon > 1023) {
         $("#myModal-employ-7").modal("show");
     }
 }
@@ -113,11 +114,201 @@ btnTrashArr.addEventListener("click", addEmploy);
 function addContact(event) {
     event.preventDefault();
     var elem = event.target;
+    var wicon = $(window).width();
     //console.log(elem);
-    if (elem.classList.contains("uservisible-str__sublink--imgcall")) {
-
+    if (elem.classList.contains("uservisible-str__sublink--imgcall") && wicon > 1023) {
+        console.log(wicon);
         $("#myModal-addcontact-7").modal("show");
     }
 }
 
 btnTrashArr.addEventListener("click", addContact);
+// Скрипт, увеличивающий блок с иконками в мобильной версии
+//const btnTrashArr = document.querySelector(".adminvisible-str");
+var addZoomIcons = void 0;
+//console.log(addZoomIcons);
+
+function zoomIconsBlock(event) {
+    event.preventDefault();
+    var elem = event.target;
+    var wicon = $(window).width();
+    var iconPopup = void 0;
+    //console.log(elem.parentNode.parentNode);
+
+    if (elem.classList.contains("uservisible-str--bottomflex") && wicon < 1024) {
+        iconPopup = elem;
+        iconPopup.classList.add("js-uservisible-str--zoomicons");
+        addZoomIcons = document.querySelector(".js-uservisible-str--zoomicons");
+
+        addZoomIcons.addEventListener("click", addZoomContact);
+
+        //console.log('btnTrashArr');
+    } else if (elem.parentNode.parentNode.classList.contains("uservisible-str--bottomflex") && wicon < 1024) {
+        iconPopup = elem.parentNode.parentNode;
+        iconPopup.classList.add("js-uservisible-str--zoomicons");
+        addZoomIcons = document.querySelector(".js-uservisible-str--zoomicons");
+        //btnTrashArr.removeEventListener("click", zoomIconsBlock);
+        addZoomIcons.addEventListener("click", addZoomContact);
+
+        //console.log('btnTrashArrParent');
+    }
+}
+
+function addZoomContact(event) {
+    event.preventDefault();
+    var elem = event.target;
+    btnTrashArr.removeEventListener("click", zoomIconsBlock);
+    if (addZoomIcons.classList.contains("js-uservisible-str--zoomicons")) {
+
+        if (elem.classList.contains("uservisible-str__sublink--imgcall")) {
+
+            addZoomIcons.classList.remove("js-uservisible-str--zoomicons");
+            //console.log('Contact');
+            $("#myModal-addcontact-7").modal("show");
+
+            setTimeout(function () {
+                btnTrashArr.addEventListener("click", zoomIconsBlock);
+            }, 1000);
+        }
+
+        if (elem.classList.contains("uservisible-str__sublink--imgman")) {
+
+            addZoomIcons.classList.remove("js-uservisible-str--zoomicons");
+            //console.log('Employ');
+            $("#myModal-employ-7").modal("show");
+
+            setTimeout(function () {
+                btnTrashArr.addEventListener("click", zoomIconsBlock);
+            }, 1000);
+        }
+    } else {
+        btnTrashArr.addEventListener("click", zoomIconsBlock);
+    }
+}
+
+btnTrashArr.addEventListener("click", zoomIconsBlock);
+// Скрипт, раскрывающий и закрывающий подменю по клику
+var userSubMenu = document.querySelector(".uservisible-str");
+
+var itemLinks = {
+    blocks: '',
+    item: '',
+    ulAll: '',
+    temp: '',
+    tempMark: '',
+    level: 1,
+    firstUlMark: ''
+};
+
+function userMenuClick(event) {
+    event.preventDefault();
+    var elem = event.target;
+    var wicon = $(window).width();
+    console.log(elem);
+
+    if (elem.parentNode.parentNode.classList.contains("uservisible-str__link-block")) {
+
+        itemLinks.blocks = elem.parentNode.parentNode;
+        itemLinks.item = itemLinks.blocks.parentNode;
+        itemLinks.ulAll = itemLinks.item.nextSibling;
+    }
+
+    if (elem.parentNode.parentNode.classList.contains("uservisible-str__sublink-block")) {
+
+        itemLinks.blocks = elem.parentNode.parentNode;
+        itemLinks.item = itemLinks.blocks.parentNode;
+        itemLinks.ulAll = itemLinks.item.nextSibling;
+        console.log(itemLinks.ulAll);
+    }
+
+    if (elem.classList.contains("uservisible-str--topflex")) {
+        itemLinks.blocks = elem.parentNode;
+        itemLinks.item = itemLinks.blocks.parentNode;
+        itemLinks.ulAll = itemLinks.item.nextSibling;
+        console.log(itemLinks.ulAll);
+    }
+
+    if (elem.classList.contains("uservisible-str--bottomflex")) {
+        itemLinks.blocks = elem.previousSibling;
+        itemLinks.item = itemLinks.blocks.parentNode.parentNode;
+        itemLinks.ulAll = itemLinks.item.nextSibling;
+        console.log(itemLinks.ulAll);
+    }
+
+    if (elem.classList.contains("uservisible-str__sublink-block")) {
+        itemLinks.blocks = elem;
+        itemLinks.item = itemLinks.blocks.parentNode;
+        itemLinks.ulAll = itemLinks.item.nextSibling;
+        console.log(itemLinks.ulAll);
+    }
+
+    if (elem.classList.contains("uservisible-str__item--mark") || elem.classList.contains("uservisible-str__item--mark-right")) {
+        itemLinks.blocks = elem;
+        itemLinks.item = itemLinks.blocks.parentNode;
+        itemLinks.ulAll = itemLinks.item.nextSibling;
+        console.log(itemLinks.ulAll);
+    }
+
+    if (wicon > 1023 && itemLinks.ulAll !== null && !elem.parentNode.parentNode.classList.contains("uservisible-str--bottomflex")) {
+        if (itemLinks.item.parentNode.parentNode.classList.contains("uservisible-str")) {
+
+            itemLinks.firstUlMark = $(".uservisible-str__item").find(".uservisible-str__item--mark-right");
+
+            if (itemLinks.firstUlMark === itemLinks.blocks) {
+
+                itemLinks.ulAll.classList.toggle("js-block");
+
+                if (itemLinks.ulAll.classList.contains("js-block")) {
+                    itemLinks.blocks.classList.remove("uservisible-str__item--mark");
+                    itemLinks.blocks.classList.add("uservisible-str__item--mark-right");
+                } else {
+
+                    itemLinks.blocks.classList.remove("uservisible-str__item--mark-right");
+                    itemLinks.blocks.classList.add("uservisible-str__item--mark");
+                }
+            } else if (itemLinks.firstUlMark.length > 0) {
+                itemLinks.firstUlMark[0].parentNode.nextSibling.classList.remove("js-block");
+                itemLinks.firstUlMark[0].classList.remove("uservisible-str__item--mark-right");
+                itemLinks.firstUlMark[0].classList.add("uservisible-str__item--mark");
+            }
+
+            itemLinks.ulAll.classList.toggle("js-block");
+
+            if (itemLinks.ulAll.classList.contains("js-block")) {
+                itemLinks.blocks.classList.remove("uservisible-str__item--mark");
+                itemLinks.blocks.classList.add("uservisible-str__item--mark-right");
+            } else {
+
+                itemLinks.blocks.classList.remove("uservisible-str__item--mark-right");
+                itemLinks.blocks.classList.add("uservisible-str__item--mark");
+            }
+        }
+    } else if (!elem.parentNode.parentNode.classList.contains("uservisible-str--bottomflex") && itemLinks.ulAll !== null) {
+        if (itemLinks.blocks.classList.contains("uservisible-str__link-block") || itemLinks.blocks.classList.contains("uservisible-str__sublink-block")) {
+
+            if (itemLinks.temp !== '' && itemLinks.temp !== itemLinks.ulAll) {
+                itemLinks.temp.classList.remove("js-block");
+                itemLinks.tempMark.classList.remove("uservisible-str__item--mark-right");
+                itemLinks.tempMark.classList.add("uservisible-str__item--mark");
+            }
+
+            itemLinks.temp = itemLinks.ulAll;
+            itemLinks.tempMark = itemLinks.blocks;
+
+            itemLinks.ulAll.classList.toggle("js-block");
+
+            if (itemLinks.ulAll.classList.contains("js-block")) {
+                itemLinks.blocks.classList.remove("uservisible-str__item--mark");
+                itemLinks.blocks.classList.add("uservisible-str__item--mark-right");
+            } else {
+
+                itemLinks.blocks.classList.remove("uservisible-str__item--mark-right");
+                itemLinks.blocks.classList.add("uservisible-str__item--mark");
+            }
+        }
+    }
+}
+
+userSubMenu.addEventListener("click", userMenuClick);
+
+userItem.addEventListener("click", userMenuClick);
