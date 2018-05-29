7d72f1fe3b9b3b236de9acb315a342a71fa6ea0a
vorobjovsh <vorobjov.sh@mail.ru>
2018-05-30 01:39:23 +0300

    correct 26

--- src/js/scripts/components/select.js
+++ src/js/scripts/components/select.js
@@ -24,7 +24,7 @@
  });
 })(jQuery);
 
-$('.select1').customStyle1();
+//$('.select1').customStyle1();
 
 // Скрипт, обрезающий количество знаков в строке (применяется на сайте для ограничения длинной строки в выпадающем списке в тэгах <option>)
 var optionText, array = [], newString, maxChar = 22;
@@ -53,5 +53,53 @@ $('select').each(function(){
 
         }
     });
+});
+
+
+/* ===== Logic for creating fake Select Boxes ===== */
+$('.sel').each(function() {
+  $(this).children('select').css('display', 'none');
+
+  var $current = $(this);
+
+  $(this).find('option').each(function(i) {
+    if (i == 0) {
+      $current.prepend($('<div>', {
+        class: $current.attr('class').replace(/sel/g, 'sel__box')
+      }));
+
+      var placeholder = $(this).text();
+      $current.prepend($('<span>', {
+        class: $current.attr('class').replace(/sel/g, 'sel__placeholder'),
+        text: placeholder,
+        'data-placeholder': placeholder
+      }));
+
+      return;
+    }
+
+    $current.children('div').append($('<span>', {
+      class: $current.attr('class').replace(/sel/g, 'sel__box__options'),
+      text: $(this).text()
+    }));
+  });
+});
+
+// Toggling the `.active` state on the `.sel`.
+$('.sel').click(function() {
+  $(this).toggleClass('active');
+});
+
+// Toggling the `.selected` state on the options.
+$('.sel__box__options').click(function() {
+  var txt = $(this).text();
+  var index = $(this).index();
+
+  $(this).siblings('.sel__box__options').removeClass('selected');
+  $(this).addClass('selected');
+
+  var $currentSel = $(this).closest('.sel');
+  $currentSel.children('.sel__placeholder').text(txt);
+  $currentSel.children('select').prop('selectedIndex', index + 1);
 });
 
