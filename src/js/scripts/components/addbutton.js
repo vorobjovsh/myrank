36e3712738aa0243f0957087d8676b7e242903d9
vorobjovsh <vorobjov.sh@mail.ru>
2018-05-29 18:50:20 +0300

    correct 18

--- src/js/scripts/components/addbutton.js
+++ src/js/scripts/components/addbutton.js
@@ -3,6 +3,10 @@ const footerBlockUser = document.querySelector(".js-footer-user");
 
 const footerBlockAdmin = document.querySelector(".js-footer-admin");
 
+const btnToggle = {
+  btn: ''
+};
+
 function addBtnFooter(event) {
     event.preventDefault();
     let elem = event.target;
@@ -13,14 +17,20 @@ function addBtnFooter(event) {
 
       elem.parentNode.parentNode.classList.toggle("footer-str__itemappl--border");
 
+      elem.classList.toggle("footer-str__btn--hover");
+
+      btnToggle.btn = elem;
+
     }
 
 
     if(elem.classList.contains("footer-str__form--addbtn")) {
 
-      elem.parentNode.parentNode.classList.remove("js-block");
+       elem.parentNode.parentNode.classList.remove("js-block");
 
        elem.parentNode.parentNode.parentNode.classList.remove("footer-str__itemappl--border");
+
+       btnToggle.btn.classList.remove("footer-str__btn--hover");
 
     }
 
